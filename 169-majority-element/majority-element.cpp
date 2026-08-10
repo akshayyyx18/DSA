c7957b8f4fd4b008b7ac1ebaa1c02e8341c505cc
akshayyyx18 <akshayyyx.18@gmail.com>
2026-08-10 22:26:51 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (94.11%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -15,15 +15,15 @@ public:
         cnt--;
     }
    }
-   int cnt1=0;
-   for(int i=0;i<nums.size();i++){
-    if(nums[i]==el){
-        cnt1++;
-    }
-   }
-    if(cnt1>(nums.size()/2)){
-        return el;
-    }
-    return -1;
+//    int cnt1=0;
+//    for(int i=0;i<nums.size();i++){
+//     if(nums[i]==el){
+//         cnt1++;
+//     }
+//    }
+//     if(cnt1>(nums.size()/2)){
+//         return el;
+//     }
+    return el;
     }
 };
